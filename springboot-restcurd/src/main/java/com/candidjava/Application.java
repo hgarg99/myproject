f0b87385471ae3074b8846024236a1bacdfaf590
hariomgarg <hgarg@astegic.com>
2020-01-08 12:43:43 +0530

    application file commited with comments

--- springboot-restcurd/src/main/java/com/candidjava/Application.java
+++ springboot-restcurd/src/main/java/com/candidjava/Application.java
@@ -3,6 +3,11 @@ package com.candidjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+author : hariom
+*/
+
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
